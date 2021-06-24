b75f6bea955d653f6f4f0f001e4d03796634f572
alexandermed <alexandermed@mail.ru>
2021-06-25 00:07:10 +0300

    task 4.2

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
 import io
 from pprint import pprint
 
-with open('recipes.txt', 'r', encoding="utf-8") as f:
-    text = f.read()
-    recipes = text.split('\n\n')
-    # print (recipes)
 
-cook_book = {}
-for item in recipes:
-    recipe = item.split('\n')
-    # print(recipe)
-    dish = recipe[0]
-    kol = recipe[1]
-    # print('Название блюда:', dish)
-    # print('Количество ингредиентов:', kol)
-    sostav = []
-    for ingredient in recipe[2:int(kol)+2]:
-        # print(ingredient)
-        ingr = ingredient.split(' | ')
-        d = {}
-        d.update({'ingredient_name': ingr[0]})
-        d.update({'quantity': ingr[1]})
-        d.update({'measure': ingr[2]})
-        sostav.append(d)
-    # print('sostav:', sostav)
-    cook_book.update({dish: sostav})
-# print(dish)
-# print(sostav)
-pprint(cook_book)
+def read_file(filename):
+    """Чтение из файла и создание словаря cook_book"""
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        text = f.read()
+        recipes = text.split('\n\n')
+
+    cook_book = {}
+    for item in recipes:
+        recipe = item.split('\n')
+        dish = recipe[0]
+        kol = recipe[1]
+        sostav = []
+        for ingredient in recipe[2:int(kol)+2]:
+            ingr = ingredient.split(' | ')
+            d = {}
+            d.update({'ingredient_name': ingr[0]})
+            d.update({'quantity': ingr[1]})
+            d.update({'measure': ingr[2]})
+            sostav.append(d)
+        cook_book.update({dish: sostav})
+    # pprint(cook_book)
+
+    return cook_book
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    """Подготовка списка ингредиентов"""
+
+    cook_book = read_file('recipes.txt')
+    shop_list = {}
+    for dish in dishes:
+        # pprint(dish)
+        for ingredient in cook_book[dish]:
+            d = {}
+            # pprint(ingredient)
+            if ingredient['ingredient_name'] in shop_list:
+                shop_list[ingredient['ingredient_name']]['quantity'] += int(ingredient['quantity']) * int(person_count)
+            else:
+                d['measure'] = ingredient['measure']
+                d['quantity'] = int(ingredient['quantity']) * int(person_count)
+                shop_list[ingredient['ingredient_name']] = d
+
+    return shop_list
+
+
+dishes = []
+# pprint(read_file('recipes.txt'))
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет', 'Фахитос'], 2))
